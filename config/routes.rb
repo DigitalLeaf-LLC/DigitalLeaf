8b80e95f18958fd015a60fe6eb96ece81b868dcf
lsaggu <lsaggu@indiana.edu>
2016-11-28 02:17:35 -0500

    adding pages and testing framework

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,13 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get 'welcome/test'
+  get 'welcome/about'
+  get 'welcome/home'
+
+  #blogs
+  get 'blog/index'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
